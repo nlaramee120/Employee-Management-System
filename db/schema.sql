6963842b1eb4b30b9f329c575f508dc079d44cc6
nlaramee120 <nlaramee120@aol.com>
2021-10-18 16:37:13 -0500

    Created and started on a schema file.

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE department (
+    id: INT AUTO_INCREMENT PRIMARY KEY;
+    name: VARCHAR(30)
+)
+
+CREATE TABLE role (
+
+)
+
+CREATE TABLE employee (
+    
+)
